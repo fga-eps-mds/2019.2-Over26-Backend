c04751e769ee417dc81cb874d91684018314778f
Erick Reis <erickcorreareis@gmail.com>
2019-09-21 23:41:28 -0300

    Adding user class.
    Signed-off-by: Erick Reis <erickcorreareis@gmail.com>

---
+++ src/models/user.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    cpf: DataTypes.BIGINT(11),
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.BIGINT(11),
+    monthly_income: DataTypes.DECIMAL(10, 2)
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
