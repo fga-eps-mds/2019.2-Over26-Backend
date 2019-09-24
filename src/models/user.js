e9890ed1b48b5fc8329dd2676c09d923ff23387c
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2019-09-24 10:21:59 -0300

    Co-authored-by: Luiza7 <luiza_cezario@hotmail.com>

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING
+    cpf: DataTypes.BIGINT(11),
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.BIGINT,
+    monthly_income: DataTypes.DECIMAL(10, 2)
   }, {});
   User.associate = function(models) {
     // associations can be defined here
