ab11219e6acfc9be28afd5b06a4873e7b17cdf1a
Luiza7 <luiza_cezario@hotmail.com>
2019-09-24 19:14:58 -0300

    Adding getInstalmentsOptions controller
    Co-authored-by: João Gabriel de Matos <joaogabriel.c.matos@gmail.com>

--- src/models/user.js
+++ src/models/user.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = function(models) {
     // associations can be defined here
     User.hasMany(models.OverdraftDebt)
+    
   };
   return User;
 };
