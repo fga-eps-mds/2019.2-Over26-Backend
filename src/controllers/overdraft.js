ab0a4e67552baf8d45bc386d1344fda6afe4d48d
CoreDoisQuad <japapauloxd@hotmail.com>
2019-10-01 21:43:58 -0300

    Created condition where limit can't be lower then limitUsed
    Co-authored-by: Gabriel Lipes <biel.lr2000@outlook.com>

--- src/controllers/overdraft.js
+++ src/controllers/overdraft.js
@@ -11,27 +11,27 @@ module.exports = {
     },
     create(req, res) {
         return User.findByPk(req.params.id)
-        .then(user=>{
-        console.log(user.cpf)
-        const status = false
-        const userCPF = user.cpf
-        const limit = 200
-        const limitMax = 200
-        const limitUsed = 0
-        const firstUseDate = null
+            .then(user => {
+                console.log(user.cpf)
+                const status = false
+                const userCPF = user.cpf
+                const limit = 200
+                const limitMax = 200
+                const limitUsed = 0
+                const firstUseDate = null
 
 
-        return user.createOverdraft({
-            userCPF: userCPF,
-            status: status,
-            limit: limit,
-            limitMax: limitMax,
-            limitUsed: limitUsed,
-            firstUseDate: firstUseDate
-        })
-            .then(overdraft => res.status(201).send(overdraft))
-            .catch(error => res.status(400).send(error));
-    })
+                return user.createOverdraft({
+                    userCPF: userCPF,
+                    status: status,
+                    limit: limit,
+                    limitMax: limitMax,
+                    limitUsed: limitUsed,
+                    firstUseDate: firstUseDate
+                })
+                    .then(overdraft => res.status(201).send(overdraft))
+                    .catch(error => res.status(400).send(error));
+            })
     },
     getByPk(req, res) {
         return Overdraft.findByPk(req.params.id)
@@ -46,6 +46,7 @@ module.exports = {
             .catch(error => res.status(400).send("error"));
     },
     updateCreditLimit(req, res) {
+        const { limit } = req.body
         return Overdraft.findByPk(req.params.id)
             .then(overdraft => {
                 if (!overdraft) {
@@ -53,6 +54,12 @@ module.exports = {
                         message: "Overdraft Not Found"
                     });
                 }
+                if ((limit - overdraft.limitUsed < 0) || (limit - overdraft.limitMax > 0)) {
+                    return res.status(400).send({
+                        message: "Limit Used exceeds choosen limit"
+                    });
+                }
+
                 return overdraft
                     .update({
                         status: req.body.status,
