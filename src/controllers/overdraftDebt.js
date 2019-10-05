ba090716044b0754087f57fa753b7049ab701bb7
isabellacgmsa <isabellacgmsa1@gmail.com>
2019-10-05 15:40:26 -0300

    Refactoring overdraftDebt's getInstalmentOptions to use overdraftDebtUtils's returnInstalmentDates. Co-authored-by: João Gabriel de Matos <joaogabriel.c.matos@gmail.com>

--- src/controllers/overdraftDebt.js
+++ src/controllers/overdraftDebt.js
@@ -78,28 +78,8 @@ module.exports = {
                         message: "OverdraftDebt Not Found"
                     });
                 }
-
-                const currentDate = new Date();
-                const dateDiff = currentDate.getTime() - overdraftDebt.entryDate.getTime();
-                const dateDiffDays = dateDiff / 86400000;
-                const dateDiffDaysRound = ((dateDiffDays).toFixed(0));
-
-                const totalAmount = overdraftDebt.amount * Math.pow(1 + overdraftDebt.rate, dateDiffDaysRound)
-                const quantityInstalment = req.query.quantityInstalment;//qunatity of instalments
-                const instalmentValue = totalAmount / quantityInstalment;//is the value of each instalment
-
-                const dueDay = req.query.day;//due day on each month for the instalments
-                var dateOptionsForInstalments = new Array();
-                var counter = 1;
-                const counterMax = parseInt(quantityInstalment, 10) + 1;
-                while (counter < counterMax) {
-
-                    dateOptionsForInstalments.push(new Date(currentDate.getFullYear(), (currentDate.getMonth() + counter), dueDay, 23, 59, 59, 999));
-                    counter++;
-
-                }
-
-
+                var instalmentValue;
+                const dateOptionsForInstalments = overdraftDebtUtils.returnInstalmentDates(req.body.day, re.body.quantityInstalment, instalmentValue, overdraftDebt.userCPF);
                 return res.status(200).send({
                     "valueOfIndividualInstalment": instalmentValue,
                     "dateOptionsForInstalments": dateOptionsForInstalments,
