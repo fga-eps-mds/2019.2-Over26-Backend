77bd2df9a8046ea04e17848e11c564cb25b4886f
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2019-10-07 22:54:03 -0300

    Adapting controllers to use "userId" instead of "userCPF".
    Signed-off-by: João Gabriel de Matos <joaogabriel.c.matos@gmail.com>

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,51 +1,51 @@
 const User = require("../models").User;
 // Controllers are used for handle any incoming URL request
 module.exports = {
-  // Create a new user
-  create(req, res) {
-    return User.create({
-      cpf: req.body.cpf,
-      name: req.body.name,
-      email: req.body.email,
-      phone: req.body.phone,
-      monthly_income: req.body.monthly_income      
-    })
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
-  },
-  // Get a user by primary key
-  getByPk(req, res) {
-    return User.findByPk(req.params.id)
-      .then(user => {
-        if (!user) {
-          return res.status(404).send({
-            message: "User Not Found"
-          });
-        }
-        return res.status(200).send(user);
-      })
-      .catch(error => res.status(400).send("error"));
-  },
-  // Get a user by primary key and update it
-  update(req, res) {
-    return User.findByPk(req.params.id)
-      .then(user => {
-        if (!user) {
-          return res.status(404).send({
-            message: "User Not Found"
-          });
-        }
-        return user
-          .update({
+    // Create a new user
+    create(req, res) {
+        return User.create({
             cpf: req.body.cpf,
             name: req.body.name,
             email: req.body.email,
             phone: req.body.phone,
-            monthly_income: req.body.monthly_income 
-          })
-          .then(user => res.status(200).send(user))
-          .catch(error => res.status(400).send(error));
-      })
-      .catch(error => res.status(400).send(error));
-  }
+            monthlyIncome: req.body.monthlyIncome
+        })
+            .then(user => res.status(201).send(user))
+            .catch(error => res.status(400).send(error));
+    },
+    // Get a user by primary key
+    getByPk(req, res) {
+        return User.findByPk(req.params.id)
+            .then(user => {
+                if (!user) {
+                    return res.status(404).send({
+                        message: "User Not Found"
+                    });
+                }
+                return res.status(200).send(user);
+            })
+            .catch(error => res.status(400).send("error"));
+    },
+    // Get a user by primary key and update it
+    update(req, res) {
+        return User.findByPk(req.params.id)
+            .then(user => {
+                if (!user) {
+                    return res.status(404).send({
+                        message: "User Not Found"
+                    });
+                }
+                return user
+                    .update({
+                        cpf: req.body.cpf,
+                        name: req.body.name,
+                        email: req.body.email,
+                        phone: req.body.phone,
+                        monthlyIncome: req.body.monthlyIncome
+                    })
+                    .then(user => res.status(200).send(user))
+                    .catch(error => res.status(400).send(error));
+            })
+            .catch(error => res.status(400).send(error));
+    }
 };
