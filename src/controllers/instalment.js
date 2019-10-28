1477da385b578d0d437e5b4ebbdccdfcaf232e86
isabellacgmsa <isabellacgmsa1@gmail.com>
2019-10-28 16:10:15 -0300

    Adding instalment payment controller
    Signed-off-by: isabellacgmsa <isabellacgmsa1@gmail.com>

--- src/controllers/instalment.js
+++ src/controllers/instalment.js
@@ -1,6 +1,23 @@
 const Instalment = require("../models").Instalment;
 // Controllers are used for handle any incoming URL request
 module.exports = {
-    
-
+    payInstalment(req, res){
+        return Instalment.findByPk(req.params.id)
+        .then(instalment=>{
+            if(!instalment){
+                return res.status(404).send({
+                    message:"Instalment Not Found"
+                })
+            }
+            return instalment.update({
+                isPaid:true
+            })
+            .then(instalment=>{
+                return res.status(200).send(instalment)
+            })
+        })
+        .catch(error=>{
+            return res.status(400).send(error)
+        })
+    }
 }
